07aebcdaaf50aff30ba871e6223a7d4391d0c2b9
Jaime Pericas <jaime.pericas.saez@gmail.com>
2023-10-09 20:47:29 -0700

    render tagid

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-/*
-Product ID:	0x0035
-Vendor ID:	0xffff
-*/
-
-// const map = {
-//   "1e21231e27211f21": "da82977e-89d8-4232-8d45-8ccddca61095",
-// }
-
-const five = require("johnny-five")
-const font = require("oled-font-5x7")
 const Oled = require("oled-js")
-
-const board = new five.Board()
-
-board.on("ready", () => {
-  console.log("Connected to Arduino, ready.")
-
-  const opts = {
-    width: 128,
-    height: 32,
-    address: 0x3c,
-  }
-
-  const oled = new Oled(board, five, opts)
-  oled.setCursor(1, 1)
-  oled.writeString(font, 1, "jaime testing new stuff", 1, true, 2)
-  oled.update()
-})
-
+const font = require("oled-font-5x7")
+const five = require("johnny-five")
 const HID = require("node-hid")
-const devices = HID.devices()
 
-const deviceInfo = devices.find(
-  (device) => device.vendorId === 65535 && device.productId === 53
-)
+/*
+  Product ID:	0x0035
+  Vendor ID:	0xffff
+  Version:	1.00
+  Serial Number:	08FF20150112
+  Speed:	Up to 1.5 Mb/s
+  Manufacturer:	Sycreader
+  Location ID:	0x00100000 / 1
+  Current Available (mA):	500
+  Current Required (mA):	100
+  Extra Operating Current (mA):	0
+*/
+const READER_VENDOR_ID = 65535 // 0x0035
+const READER_PRODUCT_ID = 53 // 0xffff
 
-if (!deviceInfo) {
-  console.log("RFID reader not found")
-  process.exit(1)
+function log(msg) {
+  console.log("[decker]", msg)
 }
 
-const rfidReader = new HID.HID(deviceInfo.path)
+function getReader() {
+  const readerInfo = HID.devices().find(
+    (device) =>
+      device.vendorId === READER_VENDOR_ID &&
+      device.productId === READER_PRODUCT_ID
+  )
+  if (!readerInfo) {
+    log("reader not found")
+    process.exit(1)
+  }
+  return new HID.HID(readerInfo.path)
+}
+
+const reader = getReader()
+const arduino = new five.Board()
+
+let oled
+arduino.on("ready", () => {
+  log("arduino connected")
+  oled = new Oled(arduino, five, {
+    address: 0x3c,
+    width: 128,
+    height: 32,
+  })
+})
+
+function write(msg) {
+  oled.setCursor(1, 1)
+  oled.writeString(font, 1, msg, 1, true, 2)
+  oled.update()
+}
 
 let tagId = ""
 
-rfidReader.on("data", (data) => {
-  const scancode = data[2].toString(16).padStart(2, "0") // Convert to 2-digit hexadecimal
+reader.on("data", (data) => {
+  // Convert to 2-digit hexadecimal:
+  const scancode = data[2].toString(16).padStart(2, "0")
 
   // Check for Enter key press (scancode 0x28)
   if (scancode === "28") {
-    console.log("Tag ID:", tagId)
+    log("tagId:", tagId)
+    write(tagId)
     tagId = "" // Reset tagId for the next read
   } else if (scancode !== "00") {
     tagId += scancode // Append scancode to tagId
   }
 })
 
-rfidReader.on("error", (err) => {
-  console.error("Error:", err)
+reader.on("error", (err) => {
+  log(err)
 })
 
-function closeResources() {
-  rfidReader.close()
-  process.exit()
-}
-
-process.on("SIGINT", () => {
-  console.log("Caught interrupt signal, cleaning up...")
-  closeResources()
+process.on("SIGINT", function () {
+  log("shutting down")
+  reader.close()
+  process.exit(0)
 })
