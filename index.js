cdb180a6bd7d745f98f162fb419a5067e833147a
Jaime Pericas <jaime.pericas.saez@gmail.com>
2023-10-09 17:29:09 -0700

    parse buffer

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 /*
 Product ID:	0x0035
 Vendor ID:	0xffff
-*/ const HID = require("node-hid")
+*/
+const HID = require("node-hid")
 const devices = HID.devices()
 
-// Log all connected HID devices to find your RFID reader
-console.log(devices)
-
-// Use the decimal values for vendorId and productId
 const deviceInfo = devices.find(
   (device) => device.vendorId === 65535 && device.productId === 53
 )
@@ -20,7 +17,8 @@ if (!deviceInfo) {
 const rfidReader = new HID.HID(deviceInfo.path)
 
 rfidReader.on("data", (data) => {
-  console.log("Data:", data)
+  const hexString = data.toString("hex")
+  console.log("Data:", hexString)
 })
 
 rfidReader.on("error", (err) => {
