8351631801c297dd08c5e4f600cfa9eeae7e2646
Jaime Pericas <jaime.pericas.saez@gmail.com>
2023-10-15 20:41:41 -0700

    read and fetch rfid

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+const axios = require("axios")
 const SerialPort = require("serialport")
+
+async function getInfoById(id) {
+  console.log("fetching:", id)
+  const map = {
+    // todo: readfilesync
+    // for cheap persistance:
+    "19.23.193.247": "da82977e-89d8-4232-8d45-8ccddca61095",
+    "35.168.196.253": "75e1eef1-0ce1-4046-96b3-4d681d53f674",
+  }
+  if (!map[id]) {
+    return "unknown rfid"
+  }
+  return await axios
+    .get("https://decksofkeyforge.com/api/decks/with-synergies/" + map[id])
+    .then((response) => {
+      const deck = response.data.deck
+      return `${deck.name}: ${deck.sasRating}SAS`
+    })
+    .catch((err) => {
+      return "axios-error:", err
+    })
+}
 
 const Reader = SerialPort.parsers.Readline
 const parser = new Reader({ delimiter: "\r\n" })
@@ -9,14 +32,13 @@ const usb = {
     nano: "/dev/cu.usbserial-110",
   },
   pi: {
-    uno: "",
-    nano: "",
+    arduino: "/dev/ttyUSB0",
   },
 }
 
 const port = new SerialPort(usb.mac.nano, {
   flowControl: false,
-  baudRate: 9600,
+  baudRate: 115200,
   parity: "none",
   dataBits: 8,
   stopBits: 1,
@@ -27,12 +49,14 @@ port.on("open", () => {
   port.pipe(parser)
 })
 
-parser.on("data", (data) => {
+parser.on("data", async (data) => {
   // arduino request:
   if (data.startsWith("get:")) {
-    // axios get data:
-    const result = "response\n"
-    port.write(result, (err) => {
+    console.log("request:", data)
+    const id = data.split("get:").pop()
+    const info = await getInfoById(id)
+    console.log("found:", info)
+    port.write(info, (err) => {
       if (err) {
         console.log("node:failure:", err.message)
         return
@@ -40,18 +64,10 @@ parser.on("data", (data) => {
       console.log("node:success")
     })
   }
-
   // arduino reports
   // back after response:
   if (data.startsWith("status:")) {
     const status = data.split("status:").pop()
-    switch (status) {
-      case "200":
-        console.log("arduino:success")
-        break
-      case "500":
-        console.log("arduino:failure")
-        break
-    }
+    console.log("ping:", status)
   }
 })
--- nano.ino
+++ nano.ino
@@ -1,31 +1,18 @@
-class Messenger
-{
-public:
-  void listen()
-  {
-    if (Serial.available() > 0)
-    {
-      String message = Serial.readString();
-      Serial.println("status:200");
-      // oled print screen
-    }
-  }
-  void get(String id)
-  {
-    Serial.println("get:request:" + id);
-  }
-};
+#include <SoftwareSerial.h>
+#include <PN532_SWHSU.h>
+#include <PN532.h>
 
-class Timer
-{
-private:
+SoftwareSerial SWSerial(3, 2);
+PN532_SWHSU pn532swhsu(SWSerial);
+PN532 nfc(pn532swhsu);
+String tagId = "None", dispTag = "None";
+byte nuidPICC[4];
+
+class Timer {
+public:
   unsigned long current = millis();
-
-public:
-  bool interval(int delay)
-  {
-    if (millis() >= current + delay)
-    {
+  bool interval(int delay) {
+    if (millis() >= current + delay) {
       current = millis();
       return true;
     }
@@ -33,21 +20,88 @@ public:
   }
 };
 
+class Messenger {
+public:
+  void listen() {
+    if (Serial.available() > 0) {
+      String message = Serial.readString();
+      Serial.println("status:recevied:" + message);
+      // oled print screen
+    }
+  }
+  void get(String id) {
+    Serial.println("get:request:" + id);
+  }
+};
 const Messenger messenger;
-const Timer timer;
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("satus:0");
+class Reader {
+public:
+  // throttle read frequency
+  const Timer listenTimer;
+
+  void prepare() {
+    nfc.begin();
+    uint32_t versiondata = nfc.getFirmwareVersion();
+    // board missing
+    if (!versiondata) {
+      Serial.println("status:cannot find rfid board");
+      return;
+    }
+    // board found
+    Serial.print("status:found rfid board");
+    Serial.println((versiondata >> 24) & 0xFF, HEX);
+    // board version
+    Serial.print("status:firmware ver:");
+    Serial.print((versiondata >> 16) & 0xFF, DEC);
+    Serial.print('.');
+    Serial.println((versiondata >> 8) & 0xFF, DEC);
+    // rfid read setup
+    nfc.SAMConfig();
+  }
+
+  void listen() {
+    if (!listenTimer.interval(2000)) {
+      return;
+    }
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+    uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    bool success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+    if (success) {
+      // rfid type
+      Serial.print("status:uid-type:");
+      Serial.print(uidLength, DEC);
+      Serial.println(" bytes");
+      // rfid value
+      for (uint8_t i = 0; i < uidLength; i++) {
+        nuidPICC[i] = uid[i];
+      }
+      tagId = toString(nuidPICC);
+      dispTag = tagId;
+      Serial.print(F("get:"));
+      Serial.println(tagId);
+    } else {
+      Serial.println("status:vacant");
+    }
+  }
+
+  String toString(byte id[4]) {
+    String tagId = "";
+    for (byte i = 0; i < 4; i++) {
+      if (i < 3) tagId += String(id[i]) + ".";
+      else tagId += String(id[i]);
+    }
+    return tagId;
+  }
+};
+const Reader reader;
+
+void setup() {
+  Serial.begin(115200);
+  reader.prepare();
 }
 
-void loop()
-{
+void loop() {
+  reader.listen();
   messenger.listen();
-
-  if (timer.interval(3000))
-  {
-    messenger.get("uuid-here");
-  }
 }
